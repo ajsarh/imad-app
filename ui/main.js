64c708cc8d1ced1395c04e8ea9c2ccd94c15e2a6
ajsarh <ajsarhisan@gmail.com>
2017-09-18 19:24:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ element.innerHTML='new Value';
 var img=document.getElementById('ajsar');
 var marginLeft=0;
 function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+01;
     img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setInterval(moveRight,100);
+    var interval=setInterval(moveRight,50);
     
 }
