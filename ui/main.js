8e1b1e6a95cdfdd3e722848926f487e51c2b0339
ajsarh <ajsarhisan@gmail.com>
2017-09-18 19:23:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,12 @@ var element = document.getElementById('main-text');
 element.innerHTML='new Value'; 
 //Move the Image
 var img=document.getElementById('ajsar');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
+    
 }
