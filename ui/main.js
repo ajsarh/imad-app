1b186a3424820d3304073d404d0d5a2b8da75be2
ajsarh <ajsarhisan@gmail.com>
2017-09-19 21:41:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,24 @@
 console.log('Loaded!');
 //Changing the HTML
 var element = document.getElementById('main-text');
-
+//Counter code
+var button=doucment.getElementById('counter')
+button.onClick = function(){
+    //Make a request to the counter
+    var request=new XMLHttpRequest();
+    //Store in a variable
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementId('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    }
+    request.open=('GET','http://ajsarhisan.imad.hasura.io/counter',true);
+    request.send=(null);
+}
 //Move the Image
 var img=document.getElementById('ajsar');
 var marginLeft=0;
