2f95e6beb2d80644b623abe55ae80da4f1d8ae36
ajsarh <ajsarhisan@gmail.com>
2017-09-23 00:44:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ var path = require('path');
 
 var app = express();
 var counter=0;
+var Pool=require('pg').Pool;
+var config={
+    user:'ajsarhisan',
+    database:'ajsarhisan',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password: process.env.DB_PASSWORD
+    
+};
+var pool=new Pool(config);
+app.get('/test-db',fuction(req,res){
+    //Make a select request
+    //Return a response
+    pool.query('SELECT * FROM test', function(err,result){
+        if (err){
+            res.status(500).send(err.toString());
+        }
+        else {
+            res.send(JSON.stringify(result));
+        }
+    })
+});
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
